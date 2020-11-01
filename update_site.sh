cc2d83ab9d95608bea1d4999ef6c6f98561d21c8
ip-default-ttl <kirik1805@gmail.com>
2020-11-01 23:29:55 +0300

    minor updates

--- update_site.sh
+++ update_site.sh
@@ -1,3 +1,3 @@
-cd /var/www/html
-sudo cp /home/ubuntu/project_601/index.nginx-debian.html index.nginx-debian.html
+cd /data/www/ip-default-ttl.ml
+sudo cp /home/ubuntu/project_601/index.html index.nginx-debian.html
 sudo systemctl restart nginx
