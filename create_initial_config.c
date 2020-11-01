e206860bb8013c8771e4d45677483090a4579fab
ip-default-ttl <kirik1805@gmail.com>
2020-11-01 16:11:37 +0300

    update create_initial_config

--- create_initial_config.c
+++ create_initial_config.c
@@ -34,10 +34,15 @@ int main()
   fout=fopen(".config","w");
   fprintf(fout,"%s\n","#This is default config file, please change parameters as you need");
   fprintf(fout,"%s\n","web_enable: 1");
+  fprintf(fout,"%s\n","log_enable: 1");
   fprintf(fout,"%s\n","#Please remember all passwords, they will be deleted after first initialization!");
   fprintf(fout,"%s\n","username: admin");
-  fprintf(fout,"%s\n","user_password: admin");
+  fprintf(fout,"%s\n","user_pass: admin");
   fprintf(fout,"%s\n","superuser: root");
-  fprintf(fout,"%s\n","superuser_password: toor");
+  fprintf(fout,"%s\n","superuser_pass: toor");
+  fprintf(fout,"%s\n","#MySQL config");
+  fprintf(fout,"%s\n","SQL_ip: 127.0.0.1");
+  fprintf(fout,"%s\n","SQL_user: root");
+  fprintf(fout,"%s\n","SQL_pass: toor");
   return 0;
 }
