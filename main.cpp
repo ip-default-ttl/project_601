15dbb5f47ae1b4cb0e502c94f63d0ecce20ba70e
ip_default_ttl <kirik1805@gmail.com>
2021-01-16 22:02:30 +0300

    plate detect

--- main.cpp
+++ main.cpp
@@ -15,21 +15,6 @@
 using namespace cv;
 using namespace std;
 
-struct cam
-{
-  string ip;
-  string port;
-  string url;
-  string login;
-  string password;
-  string proto;
-};
-
-inline void generate_url()
-{
-  cam.url=cam.proto+"://"+cam.login+':'+cam.password+'@'+cam.ip+':'+cam.port+"/live/0/MAIN";
-}
-
 int main()
 {
   //видеопоток
@@ -46,15 +31,26 @@ int main()
     imshow("main stream", edges);
     waitKey(0);
   }*/
-  Mat source_img;
+  Mat source_img=imread("test.jpeg");
+  Mat gray;
   CascadeClassifier cascadePlate;
   bool cascadeLoad = cascadePlate.load("haarcascade_russian_plate_number.xml"); // Загрузка каскада
-  if(!cascadeSymbolLoad)
+  if(!cascadeLoad)
   {
-    std::cerr << "Cascade not load. Check your directory \"haarcascade_russian_plate_number_symbol.xml\"" << std::endl;
+    cerr << "Cascade not load. Check your directory \"haarcascade_russian_plate_number_symbol.xml\"" << std::endl;
     return 0;
   }
-  Rect plate;
+  cvtColor(source_img, gray, COLOR_BGR2GRAY);
+  vector<Rect> plate;
   cascadePlate.detectMultiScale(gray, plate);
-  
+  Point rbeg;
+  Point rend;
+  for (auto& i:plate)
+  {
+  rbeg = Point(i.x, i.y);
+  rend = Point(i.x+i.width, i.y+i.height);
+  }
+  rectangle(source_img, rbeg, rend, Scalar(1,255,1), 2);
+  imshow("Пидорас найден", source_img);
+  waitKey(0);
 }
