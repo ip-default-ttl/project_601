4c8537f9ce6cd7ea5e5c13b2bc12465fbe627e8a
ip_default_ttl <kirik1805@gmail.com>
2021-01-17 13:15:55 +0300

    plate detect

--- main.cpp
+++ main.cpp
@@ -32,15 +32,15 @@ int main()
     waitKey(0);
   }*/
   Mat source_img=imread("test.jpeg");
+  Mat source_img2=imread("test.jpeg");
   Mat gray;
+  Mat gray2;
   CascadeClassifier cascadePlate;
-  bool cascadeLoad = cascadePlate.load("haarcascade_russian_plate_number.xml"); // Загрузка каскада
-  if(!cascadeLoad)
-  {
-    cerr << "Cascade not load. Check your directory \"haarcascade_russian_plate_number_symbol.xml\"" << std::endl;
-    return 0;
-  }
+  CascadeClassifier cascadePlate2;
+  cascadePlate.load("haarcascade_russian_plate_number.xml"); // Загрузка каскада
+  cascadePlate2.load("haarcascade_licence_plate_rus_16stages.xml");
   cvtColor(source_img, gray, COLOR_BGR2GRAY);
+  cvtColor(source_img2, gray2, COLOR_BGR2GRAY);
   vector<Rect> plate;
   cascadePlate.detectMultiScale(gray, plate);
   Point rbeg;
@@ -52,5 +52,14 @@ int main()
   }
   rectangle(source_img, rbeg, rend, Scalar(1,255,1), 2);
   imshow("Пидорас найден", source_img);
+  plate.clear();
+  cascadePlate.detectMultiScale(gray2, plate);
+  for (auto& i:plate)
+  {
+  rbeg = Point(i.x, i.y);
+  rend = Point(i.x+i.width, i.y+i.height);
+  }
+  rectangle(source_img2, rbeg, rend, Scalar(255,1,1), 2);
+  imshow("Пидорас найден2", source_img2);
   waitKey(0);
 }
