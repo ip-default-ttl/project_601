aaa8411ce3450cb4244bc24e22c3cb9c8d9c94cd
ip_default_ttl <kirik1805@gmail.com>
2021-01-26 22:51:27 +0300

    video stream integrated

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ using namespace std;
 int main()
 {
   //видеопоток
-  /*string url="rtsp://admin:180500Kn@172.18.18.3:554/live/0/SUB";
+  string url="rtsp://admin:180500Kn@172.18.18.3:554/live/0/SUB";
   VideoCapture cap (url);
   Mat source_img;
   while (true)
@@ -31,15 +31,15 @@ int main()
     cap>>source_img;
     namedWindow("main stream", WINDOW_AUTOSIZE);
     imshow("main stream", source_img);
-    Mat edges;
+    /*Mat edges;
     cvtColor(source_img, edges, COLOR_BGR2GRAY);
     Canny(edges, edges, 30, 60);
     namedWindow("main stream2", WINDOW_AUTOSIZE);
     imshow("main stream2", edges);
-    waitKey(0);
-  }*/
-  Mat source_img=imread("test.jpeg");
-  Mat source_img2=imread("test.jpeg");
+    waitKey(0);*/
+  //}
+  //Mat source_img=imread("test.jpeg");
+  //Mat source_img2=imread("test.jpeg");
   Mat gray;
   Mat gray2;
   CascadeClassifier cascadePlate;
@@ -47,7 +47,7 @@ int main()
   cascadePlate.load("haarcascade_russian_plate_number.xml"); // Загрузка каскада
   cascadePlate2.load("haarcascade_licence_plate_rus_16stages.xml");
   cvtColor(source_img, gray, COLOR_BGR2GRAY);
-  cvtColor(source_img2, gray2, COLOR_BGR2GRAY);
+  //cvtColor(source_img2, gray2, COLOR_BGR2GRAY);
   vector<Rect> plate;
   cascadePlate.detectMultiScale(gray, plate);
   Point rbeg;
@@ -60,7 +60,7 @@ int main()
   }
   imshow("Result", source_img);//поменять имя окна
   plate.clear();
-  cascadePlate.detectMultiScale(gray2, plate);
+  /*cascadePlate.detectMultiScale(gray2, plate);
   for (auto& i:plate)
   {
   rbeg = Point(i.x, i.y);
@@ -68,6 +68,7 @@ int main()
   rectangle(source_img2, rbeg, rend, Scalar(255,1,1), 2);
   }
   imshow("Result2", source_img2);//тут тоже
-  
-  waitKey(0);
+  waitKey(0);*/
+  waitKey(10);
+}
 }
