f9aad9163f0d1adb288bc96a128ad7c493c1c5a2
ip_default_ttl <kirik1805@gmail.com>
2021-02-07 15:45:42 +0300

    login works!

--- app.cpp
+++ app.cpp
@@ -1,33 +1,46 @@
 #include <gtk/gtk.h>
 #include <gtk/gtkx.h>
-#include <stdio.h>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
 
 //эту хуйню не удалять и в main не пихать
 GtkWidget* window;
+GtkWidget* window2;
 GtkWidget* fixed1;
+GtkWidget* fixed2;
 GtkWidget* button1;
 GtkWidget* button2;
+GtkWidget* button3;
 GtkWidget* login_entry;
 GtkWidget* password_entry;
 GtkWidget* pass_incorrect;
 GtkBuilder* builder;
 
-/*void button1_clicked(GtkButton *button, gpointer user_data)
+void on_button1_clicked(GtkButton *button, gpointer user_data)
 {
-  char* login = (char*)malloc(sizeof(char)*100);
-  char* password = (char*)malloc(sizeof(char)*100);
-  login = gtk_entry_get_text(GTK_ENTRY((GtkWidget*) login_entry));
-  password = gtk_entry_get_text(GTK_ENTRY((GtkWidget*) password_entry));
-  login = (char*)"xxx";
-  printf("%s\n",login);
-  if ((login == "admin")&&(password=="admin")) gtk_main_quit();
+  std::string login;
+  const char* log = gtk_entry_get_text(GTK_ENTRY((GtkWidget*) login_entry));
+  std::string password;
+  const char* pas = gtk_entry_get_text(GTK_ENTRY((GtkWidget*) password_entry));
+  guint16 len = gtk_entry_get_text_length(GTK_ENTRY((GtkWidget*) login_entry));
+  login.assign(log, len);
+  len = gtk_entry_get_text_length(GTK_ENTRY((GtkWidget*) password_entry));
+  password.assign(pas, len);
+  if ((login == "admin")&&(password=="admin"))
+  {
+    gtk_widget_destroy(window);
+    gtk_widget_show(window2);
+    gtk_main();
+  }
   else gtk_label_set_text(GTK_LABEL(pass_incorrect), "неправильный пароль");
-}*/
+}
 
 void on_button2_clicked(GtkButton *button, gpointer user_data)
 {
     gtk_entry_set_text(GTK_ENTRY(login_entry), "");
     gtk_entry_set_text(GTK_ENTRY(password_entry), "");
+    gtk_label_set_text(GTK_LABEL(pass_incorrect), "");
 }
 
 int main (int argc, char* argv[])
@@ -35,14 +48,19 @@ int main (int argc, char* argv[])
   gtk_init(&argc, &argv);
   builder = gtk_builder_new_from_file("app.glade");
   window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+  window2 = GTK_WIDGET(gtk_builder_get_object(builder, "window2"));
   fixed1 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed1"));
+  fixed2 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed2"));
   button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
   button2 = GTK_WIDGET(gtk_builder_get_object(builder, "button2"));
+  button3 = GTK_WIDGET(gtk_builder_get_object(builder, "button3"));
   login_entry = GTK_WIDGET(gtk_builder_get_object(builder, "login_entry"));
   password_entry = GTK_WIDGET(gtk_builder_get_object(builder, "password_entry"));
   pass_incorrect = GTK_WIDGET(gtk_builder_get_object(builder, "pass_incorrect"));
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  g_signal_connect(window2, "destroy", G_CALLBACK(gtk_main_quit), NULL);
   g_signal_connect(GTK_BUTTON(button2), "clicked", G_CALLBACK(on_button2_clicked), NULL);
+  g_signal_connect(GTK_BUTTON(button1), "clicked", G_CALLBACK(on_button1_clicked), NULL);
   gtk_widget_show(window);
   gtk_main();
   return 0;
