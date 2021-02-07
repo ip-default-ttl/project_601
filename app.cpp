05f1937b02bfc9e7d5e64397ee01963ba5a7eb5c
ip_default_ttl <kirik1805@gmail.com>
2021-02-08 00:17:50 +0300

    add new features

--- app.cpp
+++ app.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
 //виджеты первого окна
 GtkWidget* window;
@@ -16,8 +17,19 @@ GtkWidget* pass_incorrect;
 GtkWidget* window2;
 GtkWidget* fixed2;
 GtkWidget* button3;
+GtkWidget* toggle1;
+GtkWidget* button4;
 //какая-то неведомая поебень
 GtkBuilder* builder;
+//данные датчиков
+int temp;
+
+inline int getRandomNumber(int min, int max)
+{
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+//блять ну и язык, даже рандома без static_cast не напишешь
 
 void on_button1_clicked(GtkButton *button, gpointer user_data)
 {
@@ -45,9 +57,23 @@ void on_button2_clicked(GtkButton *button, gpointer user_data)
     gtk_label_set_text(GTK_LABEL(pass_incorrect), "");
 }
 
+void toggle1_toggled(GtkButton *button, gpointer user_data)
+{
+    std::cout<<"1"<<std::endl;
+}
+
+void test_func()
+{
+  temp = getRandomNumber(0,30);
+  std::string new_label = "Темп: ";
+  new_label+=std::to_string(temp);
+  gtk_button_set_label(GTK_BUTTON((GtkWidget*) button4), new_label.c_str());
+}
+
 int main (int argc, char* argv[])
 {
   gtk_init(&argc, &argv);
+  srand(time(0));
   builder = gtk_builder_new_from_file("app.glade");
   window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
   window2 = GTK_WIDGET(gtk_builder_get_object(builder, "window2"));
@@ -56,13 +82,17 @@ int main (int argc, char* argv[])
   button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
   button2 = GTK_WIDGET(gtk_builder_get_object(builder, "button2"));
   button3 = GTK_WIDGET(gtk_builder_get_object(builder, "button3"));
+  button4 = GTK_WIDGET(gtk_builder_get_object(builder, "button4"));
   login_entry = GTK_WIDGET(gtk_builder_get_object(builder, "login_entry"));
   password_entry = GTK_WIDGET(gtk_builder_get_object(builder, "password_entry"));
   pass_incorrect = GTK_WIDGET(gtk_builder_get_object(builder, "pass_incorrect"));
+  toggle1 = GTK_WIDGET(gtk_builder_get_object(builder, "toggle1"));
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
   g_signal_connect(window2, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  g_signal_connect(toggle1, "toggled", G_CALLBACK(toggle1_toggled), NULL);
   g_signal_connect(GTK_BUTTON(button2), "clicked", G_CALLBACK(on_button2_clicked), NULL);
   g_signal_connect(GTK_BUTTON(button1), "clicked", G_CALLBACK(on_button1_clicked), NULL);
+  g_timeout_add (10000, GSourceFunc(test_func), NULL);
   gtk_widget_show(window);
   gtk_main();
   return 0;
